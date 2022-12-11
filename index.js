4e08acd037e3ddf39e5dfc8308475f3704e7eef8
Javier Fernandez <jafernandez1219@gmail.com>
2022-12-11 00:34:57 -0500

    added search selection and jump features
    Added a feature where the user can pick between 5 songs that the bot searched for. Another feature was added for the User to be able to skip ahead to whatever song given the queue order #

--- index.js
+++ index.js
@@ -9,10 +9,14 @@ const client = new discord.Client({
     ]
 })
 
-const { DisTube } = require("distube") 
+const { DisTube, default: dist } = require("distube") 
 const {SpotifyPlugin } = require("@distube/spotify");
 
 client.DisTube = new DisTube(client, {
+  searchSongs: 5,
+  searchCooldown: 30,
+  leaveOnStop: false,
+  leaveOnFinish: false,
   leaveOnStop: false,
   emitNewSongOnly: true,
   emitAddSongWhenCreatingQueue: false,
@@ -26,6 +30,7 @@ client.on("ready", client => {
 })
 
 client.on("messageCreate", message => {
+    console.log(message)
     if (message.author.bot || !message.guild) return;
     const prefix = "?"
     const args = message.content.slice(prefix.length).trim().split(/ +/g)
@@ -40,6 +45,7 @@ client.on("messageCreate", message => {
             textChannel: message.channel,
             message
         })
+        message
     }
     if(command === "stop" || command === "s"){
         client.DisTube.stop(message)
@@ -50,6 +56,11 @@ client.on("messageCreate", message => {
         message.channel.send("Say peace!");
     }
 
+    if (command == "jump"){
+        client.DisTube.jump(message, parseInt(args[0]))
+            .catch(err => message.channel.send("Invalid Number"));
+    }
+
     if( command === "resume" || command === "r") client.DisTube.resume(message);
 
     if( command === "pause") client.DisTube.pause(message);
@@ -58,6 +69,7 @@ client.on("messageCreate", message => {
 
     if(command == 'queue' || command === "q"){
         const queue = client.DisTube.getQueue(message);
+       // console.log(client.DisTube.getQueue(message))
         if(!queue){
             message.channel.send('Nothing in the Q E U E');
         }else {
@@ -80,6 +92,55 @@ client.on("messageCreate", message => {
 client.DisTube.on("playSong", (queue, song) => {
     queue.textChannel.send("NOW PLAYING " + song.name)
 })
+
+
+client.DisTube.on("addSong", (queue, song) => 
+    queue.textChannel?.send(`${song.user} added this shitty song ${song.name} - It's gonna be a tough \`${song.formattedDuration}\``,
+    ),
+)
+
+client.DisTube.on('error', (textChannel, e) => {
+    console.error(e);
+    textChannel.send(
+        `Welp. Something went wrong: ${e.message.slice(0, 2000)}`,
+    );
+})
+
+
+client.DisTube.on('searchResult', (message, result) => {
+    let i = 0;
+    message.channel.send(
+        `**Choose one of these daddy uwu**\n${result
+            .map(
+                song =>
+                    `**${++i}**. ${song.name} - \`${
+                        song.formattedDuration
+                    }\``,
+            )
+            .join(
+                '\n',
+            )}\n*Enter something else or wait 30 seconds and you'll lose it!*`,
+    );
+})
+client.DisTube.on('finish', queue => queue.textChannel?.send('Finish queue!'))
+
+client.DisTube.on('finishSong', queue =>
+    queue.textChannel?.send('Finish song!'),
+)
+
+client.DisTube.on('searchCancel', message =>
+    message.channel.send('Bruh... you took too long...'),
+)
+
+client.DisTube.on('searchInvalidAnswer', message =>
+    message.channel.send('Nope. Not enough results.'),
+)
+
+client.DisTube.on('searchNoResult', message =>
+    message.channel.send('Try looking elswhere, comrade'),
+)
+
+client.DisTube.on('searchDone', () => {});
 
 require('dotenv').config();
 const token = process.env.DISCORD_TOKEN;
